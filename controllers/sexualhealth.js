f8b0ddbe6dda79883230fbc372b3a39bcc946306
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-15 14:11:04 -0700

    chlam/gono working

--- controllers/sexualhealth.js
+++ controllers/sexualhealth.js
@@ -5,6 +5,7 @@ const axios = require('axios')
 const app = require('express')
 
 const syphilisUrl = `https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=syphilis`
+const chlamGonoUrl = `https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=gonorrhea`
 
 
 
@@ -13,12 +14,25 @@ router.get('/', (req, res) => {
 })
 
 router.get('/syphilis', (req,res) => {
-    axios.get('https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=syphilis')
+    axios.get(syphilisUrl)
     .then((response)=>{
         res.render('./sexualhealth/syphilis.ejs', {
             syphilisData: response.data.Result.Resources.Resource[0].Sections.section[0].Content
         }
  )
+    })
+})
+
+router.get('/chlamydia_gonorrhea', (req,res) => {
+    axios.get(chlamGonoUrl)
+    .then((response)=>{
+        res.render('./sexualhealth/chlamydia_gonorrhea', {
+            dataPoint1: response.data.Result.Resources.Resource[0].Sections.section[0].Content,
+            dataPoint2: response.data.Result.Resources.Resource[0].Sections.section[1].Content,
+            dataPoint3: response.data.Result.Resources.Resource[0].Sections.section[2].Content,
+            dataPoint4: response.data.Result.Resources.Resource[0].Sections.section[3].Content,
+            dataPoint5: response.data.Result.Resources.Resource[0].Sections.section[4].Content
+        })
     })
 })
 
