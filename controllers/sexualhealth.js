5ce771d38c428ae38c97be9cb53f71aed72f4672
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-15 12:34:57 -0700

    api data showing up correctly

--- controllers/sexualhealth.js
+++ controllers/sexualhealth.js
@@ -7,23 +7,18 @@ const app = require('express')
 const syphilisUrl = `https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=syphilis`
 
 
-// router.get('/', async (req, res) => {
-//     try {const syphilisResponse = await axios.get(syphilisUrl)
-
-//     const syphilisData = await axios.get(syphilisResponse.data)
-
-//       res.render('./sexualhealth/home.ejs', {
-//         syphilis: syphilisData
-//       })
-//     } catch(err) {
-//         console.log(err)
-//     }
-//    } )
 
 router.get('/', (req, res) => {
+        res.render('./sexualhealth/home.ejs')
+})
+
+router.get('/syphilis', (req,res) => {
     axios.get('https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=syphilis')
     .then((response)=>{
-        res.send(response.data.Result.Resources.Resource[0].Sections.section[0].Content)
+        res.render('./sexualhealth/syphilis.ejs', {
+            syphilisData: response.data.Result.Resources.Resource[0].Sections.section[0].Content
+        }
+ )
     })
 })
 
