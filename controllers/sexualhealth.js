2a3781640e6ce00392a58a6c5b0847e254fa2860
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-15 14:38:13 -0700

    hiv api route working

--- controllers/sexualhealth.js
+++ controllers/sexualhealth.js
@@ -6,6 +6,7 @@ const app = require('express')
 
 const syphilisUrl = `https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=syphilis`
 const chlamGonoUrl = `https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=gonorrhea`
+const hivUrl = `https://health.gov/myhealthfinder/api/v3/topicsearch.json?categoryId=28&keyword=hiv`
 
 
 
@@ -34,6 +35,21 @@ router.get('/chlamydia_gonorrhea', (req,res) => {
             dataPoint5: response.data.Result.Resources.Resource[0].Sections.section[4].Content
         })
     })
+})
+
+router.get('/hiv', (req, res) => {
+    axios.get(hivUrl)
+    .then((response)=> {
+        res.render('./sexualhealth/hiv', {
+            dataPoint1: response.data.Result.Resources.Resource[0].Sections.section[0].Content,
+            dataPoint2: response.data.Result.Resources.Resource[0].Sections.section[1].Content,
+            dataPoint3: response.data.Result.Resources.Resource[0].Sections.section[2].Content,
+            dataPoint4: response.data.Result.Resources.Resource[0].Sections.section[3].Content,
+            dataPoint5: response.data.Result.Resources.Resource[0].Sections.section[4].Content,
+            dataPoint6: response.data.Result.Resources.Resource[0].Sections.section[5].Content,
+            dataPoint7: response.data.Result.Resources.Resource[0].Sections.section[6].Content
+        })
+})
 })
 
 module.exports = router
