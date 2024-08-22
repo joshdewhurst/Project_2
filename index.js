b3789df6dd7ef63f017c97eee08fac6af1326d02
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2024-08-21 21:43:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const db = require('./models')
 const crypto = require('crypto-js')
 const methodOverride = require('method-override')
 const axios = require('axios')
-const url = 'https://ghoapi.azureedge.net/api/Dimension'
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
 
 console.log('server secret:', process.env.ENC_SECRET)
 
@@ -16,35 +17,34 @@ const app = express()
 const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-// our custom auth middleware
+app.use(helmet()) // Add security headers
+
+// Rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+})
+app.use(limiter)
+
+// Custom auth middleware
 app.use(async (req, res, next) => {
-    // console.log('hello from a middleware 👋')
     res.locals.myData = 'hello, fellow route!'
-    // if there is a cookie on the incoming request
     if (req.cookies.userId) {
-        // decrypt the user id before we look up the user in the db
         const decryptedId = crypto.AES.decrypt(req.cookies.userId.toString(), process.env.ENC_SECRET)
         const decryptedIdString = decryptedId.toString(crypto.enc.Utf8)
-        // look up the user in the db
         const user = await db.user.findByPk(decryptedIdString)
-        // mount the user on the res.locals
         res.locals.user = user
-    // if there is no cookie -- set the user to be null in the res.locals
     } else {
         res.locals.user = null
     }
-    // move on to the next route or middleware in the chain
     next()
 })
 
-
-// route definitions
+// Route definitions
 app.get('/', (req, res) => {
-    // console.log('incoming cookie 🍪', req.cookies)
-    // console.log(res.locals.myData)
     console.log('the currently logged in user is:', res.locals.user)
     res.render('home.ejs')
 })
@@ -55,5 +55,11 @@ app.use('/encounters', require('./controllers/encounters'))
 app.use('/partners', require('./controllers/partners'))
 app.use('/sexualhealth', require('./controllers/sexualhealth'))
 
-// listen on a port
-app.listen(PORT, () => console.log(`you or your loved ones may be entitled to compensation on port: ${PORT}`))
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something went wrong! Please try again later.')
+})
+
+// Listen on a port
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
